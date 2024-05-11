fef757db11a07ad97debc1c463f509798165d809
maxachis <maxachis@gmail.com>
2024-05-11 18:06:06 -0400

    Create test_data_requests base

---
+++ tests/middleware/test_data_requests.py
@@ -0,0 +1,41 @@
+
+
+
+
+def test_create_request():
+
+    # Run create_request operation
+
+    # Confirm request exists in database
+
+    pass
+
+def test_read_request():
+
+    # Create test request
+
+    # Run read_request operation
+
+    # Confirm data matches
+
+    pass
+
+def test_update_request():
+
+    # Create test request
+
+    # Run update_request operation
+
+    # Confirm test request is updated
+
+    pass
+
+def test_delete_request():
+
+    # Create test request
+
+    # Run delete_request operation
+
+    # Confirm test request is deleted
+
+    pass
