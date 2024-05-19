4330465c874eb25f0fd5c4fca679af42b9fd8ef1
maxachis <maxachis@gmail.com>
2024-05-19 18:26:37 -0400

    Refactor middleware tests with new mock request setup
    The test suite in middleware for data requests has been overhauled. It now integrates a new setup for mock test data, retiring previous testing methods. This change also includes the use of a new test client and the adoption of SQLAlchemy's ORM features for testing database interactions.

--- tests/middleware/test_data_requests.py
+++ tests/middleware/test_data_requests.py
@@ -1,41 +1,30 @@
+import json
+from unittest.mock import patch
+from conftest import test_client, session
+
+import pytest
+
+from middleware.database_enums import RecordType
 
 
+@pytest.fixture
+def mock_request_info():
+    return {
+        'submission_notes': 'Test notes',
+        'submitter_contact_info': 'contact@example.com',
+        'submitter_user_id': 'user123',
+        'agency_described_submitted': 'Agency',
+        'record_type': RecordType.ARREST_RECORDS.value
+    }
 
 
-def test_create_request():
-
-    # Run create_request operation
-
-    # Confirm request exists in database
-
-    pass
-
-def test_read_request():
-
-    # Create test request
-
-    # Run read_request operation
-
-    # Confirm data matches
-
-    pass
-
-def test_update_request():
-
-    # Create test request
-
-    # Run update_request operation
-
-    # Confirm test request is updated
-
-    pass
-
-def test_delete_request():
-
-    # Create test request
-
-    # Run delete_request operation
-
-    # Confirm test request is deleted
-
-    pass
+# @patch('app.db.session.add')
+# @patch('app.db.session.commit')
+def test_post_request(test_client, mock_request_info, session):
+    response = test_client.post(
+        '/data-request',
+        data=json.dumps(mock_request_info),
+        content_type='application/json'
+    )
+    assert response.status_code == 201
+    assert response.json['message'] == 'Created successfully'
