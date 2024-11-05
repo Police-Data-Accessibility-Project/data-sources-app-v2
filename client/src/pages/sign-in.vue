546955be632a05a60bb08f11aee2e172db1c5d9d
Joshua Graber <joshua.d.graber@gmail.com>
2024-11-05 08:37:02 -0500

    refactor: login with github

--- client/src/pages/sign-in.vue
+++ client/src/pages/sign-in.vue
@@ -1,86 +1,125 @@
 <template>
 	<main class="pdap-flex-container mx-auto max-w-2xl pb-24">
 		<h1>Sign In</h1>
-		<Button
-			class="border-2 border-neutral-950 border-solid [&>svg]:ml-0"
-			intent="tertiary"
-			@click="async () => await auth.loginWithGithub()"
-		>
-			<FontAwesomeIcon :icon="faGithub" />
-			Sign in with Github
-		</Button>
 
-		<h2>Or sign in with email</h2>
-		<FormV2
-			id="login"
-			class="flex flex-col"
-			data-test="login-form"
-			name="login"
-			:error="error"
-			:schema="VALIDATION_SCHEMA"
-			@submit="onSubmit"
-		>
-			<InputText
-				id="email"
-				autocomplete="email"
-				data-test="email"
-				name="email"
-				label="Email"
-				type="text"
-				placeholder="Your email address"
-			/>
-			<InputPassword
-				id="password"
-				autocomplete="password"
-				data-test="password"
-				name="password"
-				label="Password"
-				type="password"
-				placeholder="Your password"
-			/>
-
-			<Button
-				class="max-w-full"
-				:is-loading="loading"
-				type="submit"
-				data-test="submit-button"
-			>
-				<Spinner v-if="loading" :show="loading" />
-				{{ !loading ? 'Sign in' : '' }}
-			</Button>
-		</FormV2>
 		<div
-			class="flex flex-col items-start sm:flex-row sm:items-center sm:gap-4 w-full"
+			v-if="githubLoading"
+			class="flex items-center justify-center h-full w-full"
 		>
-			<RouterLink
-				class="pdap-button-secondary flex-1 max-w-full"
-				intent="secondary"
-				data-test="toggle-button"
-				to="/sign-up"
-			>
-				Create Account
-			</RouterLink>
-			<RouterLink
-				class="pdap-button-secondary flex-1 max-w-full"
-				data-test="reset-link"
-				to="/reset-password"
-			>
-				Reset Password
-			</RouterLink>
+			<Spinner :show="githubLoading" text="Logging in" />
 		</div>
+
+		<template v-else>
+			<template v-if="githubAuthError">
+				<p class="error">
+					There was an error logging you in with Github. Please try again
+				</p>
+			</template>
+			<template v-else>
+				<template v-if="githubAuthData.userExists">
+					<p class="error">
+						You already have an account with this email address. Please log in
+						and link your existing account to Github from your profile.
+					</p>
+				</template>
+
+				<Button
+					class="border-2 border-neutral-950 border-solid [&>svg]:ml-0"
+					intent="tertiary"
+					:disabled="githubAuthData.userExists"
+					@click="async () => await auth.beginOAuthLogin()"
+				>
+					<FontAwesomeIcon :icon="faGithub" />
+					Sign in with Github
+				</Button>
+			</template>
+
+			<h2>Or sign in with email</h2>
+			<FormV2
+				id="login"
+				class="flex flex-col"
+				data-test="login-form"
+				name="login"
+				:error="error"
+				:schema="VALIDATION_SCHEMA"
+				@submit="onSubmit"
+			>
+				<InputText
+					id="email"
+					autocomplete="email"
+					data-test="email"
+					name="email"
+					label="Email"
+					type="text"
+					placeholder="Your email address"
+				/>
+				<InputPassword
+					id="password"
+					autocomplete="password"
+					data-test="password"
+					name="password"
+					label="Password"
+					type="password"
+					placeholder="Your password"
+				/>
+
+				<Button
+					class="max-w-full"
+					:is-loading="loading"
+					type="submit"
+					data-test="submit-button"
+				>
+					<Spinner v-if="loading" :show="loading" />
+					{{ !loading ? 'Sign in' : '' }}
+				</Button>
+			</FormV2>
+			<div
+				class="flex flex-col items-start sm:flex-row sm:items-center sm:gap-4 w-full"
+			>
+				<RouterLink
+					class="pdap-button-secondary flex-1 max-w-full"
+					intent="secondary"
+					data-test="toggle-button"
+					to="/sign-up"
+				>
+					Create Account
+				</RouterLink>
+				<RouterLink
+					class="pdap-button-secondary flex-1 max-w-full"
+					data-test="reset-link"
+					to="/reset-password"
+				>
+					Reset Password
+				</RouterLink>
+			</div>
+		</template>
 	</main>
 </template>
 
 <script>
-// Navigation guard via data loader
+// Data loader - navigation guard and GH auth handling
 import { NavigationResult } from 'unplugin-vue-router/data-loaders';
 import { defineBasicLoader } from 'unplugin-vue-router/data-loaders/basic';
 import { useAuthStore } from '@/stores/auth';
 
-const { userId } = useAuthStore();
+const { redirectTo, userId, loginWithGithub } = useAuthStore();
 
-export const useDataSourceData = defineBasicLoader('/sign-in', async () => {
-	if (userId) return new NavigationResult({ path: '/' });
+export const useGithubAuth = defineBasicLoader('/sign-in', async (route) => {
+	if (userId) return new NavigationResult({ path: redirectTo ?? '/' });
+
+	try {
+		const githubAccessToken = route.query.gh_access_token;
+
+		if (githubAccessToken) {
+			const tokens = await loginWithGithub(githubAccessToken);
+
+			if (tokens) return new NavigationResult({ path: redirectTo ?? '/' });
+		}
+	} catch (error) {
+		if (error.response.data.message.includes('already exists')) {
+			return { userExists: true };
+		} else throw error;
+	}
 });
 </script>
 
@@ -130,6 +169,11 @@ const VALIDATION_SCHEMA = [
 
 // Store
 const auth = useAuthStore();
+const {
+	data: githubAuthData,
+	error: githubAuthError,
+	loading: githubLoading,
+} = useGithubAuth();
 
 // Reactive vars
 const error = ref(undefined);
@@ -155,3 +199,9 @@ async function onSubmit(formValues) {
 	}
 }
 </script>
+
+<style scoped>
+.error {
+	@apply border-red-800 dark:border-red-300 items-center justify-start flex bg-red-300 text-red-800 p-1 text-sm rounded-sm max-w-full p-2;
+}
+</style>
